3ef4290a38e4ab0d9005aa0c695f32b7bc77b3a6
Cosmin <cosmin@debian.home>
2020-04-01 02:15:51 +0300

    Updates

---
+++ precache-manifest.1d8823ca2dff9d95217902b04748bf1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc34246887d19eafd03bd4f68a69503",
+    "url": "/aprozar-la-garaj/index.html"
+  },
+  {
+    "revision": "9a94e7433ae36879a69c",
+    "url": "/aprozar-la-garaj/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "9974b26d7b6dc5964fe8",
+    "url": "/aprozar-la-garaj/static/css/main.8a0bfa93.chunk.css"
+  },
+  {
+    "revision": "9a94e7433ae36879a69c",
+    "url": "/aprozar-la-garaj/static/js/2.e34052b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aprozar-la-garaj/static/js/2.e34052b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9974b26d7b6dc5964fe8",
+    "url": "/aprozar-la-garaj/static/js/main.712cf18d.chunk.js"
+  },
+  {
+    "revision": "74da1ac119ad8e80ac75",
+    "url": "/aprozar-la-garaj/static/js/runtime-main.1baec8af.js"
+  },
+  {
+    "revision": "657bfcefdd0c87a82d06a43dd2edfb73",
+    "url": "/aprozar-la-garaj/static/media/home.657bfcef.jpeg"
+  }
+]);
