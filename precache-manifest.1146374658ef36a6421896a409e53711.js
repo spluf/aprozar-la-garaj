f78b3a04474c37142b4927732d14f6cb579c77ff
Cosmin <cosmin@debian.home>
2020-04-08 21:39:45 +0300

    Updates

---
+++ precache-manifest.1146374658ef36a6421896a409e53711.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a302a7828ec5f22c876a7ecd7d2b21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9c7db73ec5565a053d8",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b2e44bbb5338067675c6",
+    "url": "/static/css/main.07dfb895.chunk.css"
+  },
+  {
+    "revision": "f9c7db73ec5565a053d8",
+    "url": "/static/js/2.8c39064c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c39064c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e44bbb5338067675c6",
+    "url": "/static/js/main.8dceaefd.chunk.js"
+  },
+  {
+    "revision": "a5ff616e7e9e3434f62d",
+    "url": "/static/js/runtime-main.3eb6adb6.js"
+  },
+  {
+    "revision": "657bfcefdd0c87a82d06a43dd2edfb73",
+    "url": "/static/media/home.657bfcef.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aprozar-la-garaj/precache-manifest.69a176b40e6a8356ab0e17e2bb8b64ed.js"
+  "/precache-manifest.1146374658ef36a6421896a409e53711.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aprozar-la-garaj/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
