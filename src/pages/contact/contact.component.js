c2238100fe8bf483aa00a181ab5bebc2379b4f5c
spluf <cosmin.s.pop@gmail.com>
2020-04-17 19:19:15 +0300

    added google maps api

--- src/pages/contact/contact.component.js
+++ src/pages/contact/contact.component.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-export default function ContactPage() {
+import './contact.styles.scss';
+
+const ContactPage = ({google}) => {
+    const mapStyles = {
+        width: '100%',
+        height: '60%',
+        margin: 'auto 0 auto',
+        zIndex: '1'
+      };
+      
     return (
-        <div className='main cf w-100'>
-            
+        <div className='main contact-page'>
+            <div className='map'>
+                <Map 
+                    style={mapStyles}
+                    google={google}
+                    zoom={8}
+                    initialCenter={{ lat: 47.444, lng: -122.176}}
+                />
+            </div>
+            <div class='contact-info'>
+                Contact info
+            </div>
         </div>
     )
 }
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDmKtCd1Ad3rsSdL0EJUGVn1W0ZaE39__0'
+  })(ContactPage);
+  
